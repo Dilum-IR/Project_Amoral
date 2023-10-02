80fe1c06d150b631edd4be05a80ca48a72f91810
LearnCsWithDIR <rdinduwara19158@gmail.com>
2023-10-03 02:38:24 +0530

    create toast message part

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -3,8 +3,13 @@
 class Controller
 {
 
-    public function view($name)
+    public function view($name,$data=[])
     {
+
+        if (!empty($data)) {
+            extract($data);
+            
+        }
 
         $filename =  "../app/views/" . $name . ".view.php";
         if (file_exists($filename)) {
