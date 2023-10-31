4f8f316eb9886369a4e766cfe5984e143c9afac0
Thiran Sasanka <thiransasanka1@gmail.com>
2023-11-01 04:08:02 +0530

    addedhome page,

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -4,27 +4,6 @@ class Home extends Controller
 {
     public function index()
     {
-        // require_once 'Model.php';
-        // echo "this is a home controller";
-
-       $user = new User;
-       $result= $user->findAll(); 
-        
-        // $arr['first_name'] = 'dilum';
-        // $result= $model->where($arr);
-
-        // $arr['fullname'] = 'thiran';
-        // $arr['email'] = 'thiran@gmail.com';
-        // $arr['password'] = '2536';
-        // $result= $user->insert($arr);
-        
-        // $result= $model->delete(5);
-        
-    //    $arr['id'] = 2;
-    //     $arr['first_email'] = 'thiran';
-        
-       show($result);
-
         $this->view('home/home');
     }
 }
--- app/views/home/home.view.php
+++ app/views/home/home.view.php
@@ -7,8 +7,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Amoral</title>
-    <link rel="stylesheet" href="<?= ROOT ?>/assets/css/home.css">
-    <link rel="stylesheet" href="<?= ROOT ?>/assets/css/style-bar.css">
+    <link rel="stylesheet" href="<?= ROOT ?>/assets/css/home/nav.css">
+    <link rel="stylesheet" href="<?= ROOT ?>/assets/css/home/home.css">
+    <link rel="stylesheet" href="<?= ROOT ?>/assets/css/home/slider.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+
 
 </head>
 
@@ -17,6 +23,42 @@
 
     <a href="<?= ROOT ?>/signin">SignIN</a>
     <h1> Home file view</h1>
+    
+    <section id="welcome-section" class="welcome-section">
+                <div class="welcome-text">
+                    <h2>
+                        Welcome to AMORAL
+                    </h2>
+                    <h3>
+                        Welcome to Amoral, your one-stop destination for custom-designed T-shirts that perfectly reflect your unique style and preferences. <a href="<?= ROOT ?>/signin" class="text-nav-link">Register</a> now to place your order. (If you have a garment factory you can <a href="#" class="text-nav-link">join with us</a>. Let's work together. Join us now.)
+                    </h3>
+
+                    <br>
+                    <a href="<?= ROOT ?>/signin" class="home-nav-link">
+                        <i class='bx bxs-user icon'></i>
+                        Sign Up
+                        <!-- <span class="chat-notification">8</span> -->
+                    </a>
+                    <br>
+                    <a href="#" class="home-partnership-link">
+                        <i class='bx bxs-file icon'></i>
+                        Partnership with AMORAL
+                        <!-- <span class="chat-notification">8</span> -->
+                    </a>
+
+
+
+                </div>
+                <div class="main-img">
+                    <img class="welcome-img" src="<?= ROOT ?>/assets/images/signin-up/4.jpg">
+                    <!-- <img class="welcome-img" src="3.jpg">
+                    <img class="welcome-img" src="2.jpg">
+                    <img class="welcome-img" src="1.jpg"> -->
+                </div>
+            </section>
+
+
+        <script src="<?= ROOT ?>/assets/js/home.js"></script>
 
 </body>
 
