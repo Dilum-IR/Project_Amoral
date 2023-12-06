98b792e153bc5c26a893d80c5c861caca8a29cc0
LumbiniRandika <2021cs159@stu.ucsc.cmb.ac.lk>
2023-12-06 21:21:39 +0530

    add notification box

--- app/views/customer/navigationbar.php
+++ app/views/customer/navigationbar.php
@@ -2,10 +2,10 @@
     <section class="home-section" id="navbar">
         <nav>
             <div class="nav-icons">
-                <a href="#" class="nav-link">
+                <div class="nav-link" onclick="showNotifi()">
                     <i class='bx bxs-bell-ring icon'></i>
                     <span class="bell-notification">5</span>
-                </a>
+</div>
                 <a href="#" class="nav-link">
                     <i class='bx bxs-chat icon'></i>
                     <span class="chat-notification">8</span>
@@ -15,5 +15,16 @@
           <img src="https://images.unsplash.com/photo-1517841905240-472988babdf9?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cGVvcGxlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="">
         </a> -->
             </div>
+
+            <div class="notifi-box">
+                <h2>Notifications <span>5</span></h2>
+                <div class="notifi-item">
+                    <i class='bx bx-right-arrow-alt'></i>
+                    <h4>This is a notification</h4>
+                </div>
+            </div>
         </nav>
     </section>
+
+    <script src="<?= ROOT ?>/assets/js/nav-bar.js"></script>
+
