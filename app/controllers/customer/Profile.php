55db2aa63e9fffe66b5ab64dbe62e6b78dd574ca
Sadeep Chathushan <chathushan43sadeep@gmail.com>
2024-01-04 21:48:42 +0530

    Change customer profile ui and some backend

--- app/controllers/customer/Profile.php
+++ app/controllers/customer/Profile.php
@@ -8,11 +8,62 @@ class Profile extends Controller
         $username = empty($_SESSION['USER']) ? 'User' : $_SESSION['USER']->email;
 
         if ($username != 'User') {
-            
+
+            $employee = new Employee;
+
+            if($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['save'])){
+                unset($_POST['save']);
+                // show($_POST);
+                $this->changeInfo($_POST,$_SESSION['USER']->emp_id,$employee);
+            }
+
+            if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['saveP'])) {
+
+                unset($_POST['saveP']);
+                $this->changePassword($_POST, $_SESSION['USER']->emp_id, $employee);
+                //show($_POST);
+            }
+
+             // get the session user data for bind to the profile info
+             $data = $this->userInfo($_SESSION['USER']->emp_id, $employee);
+
+
+             // show($data);
+
             $this->view('customer/profile');
        
         }else{
             redirect('home');
         }
+    }
+
+    private function userInfo($id,$employee){
+
+        $arr['emp_id'] = $id;
+
+        $row = $employee->first($arr);
+
+        unset($row->password);
+        unset($row->emp_id);
+        
+        $data = ['data' => $row];
+        return $data;
+        //show($row);
+
+        // $emprow = $employee->first($arr);
+    }
+
+    //  user chanage info data
+    private function changeInfo($data, $id, $employee)
+    {
+
+        // show($data);
+
+    }
+
+    //  user chanage password
+    private function changePassword($data, $id, $employee)
+    {
+
     }
 }
--- app/views/customer/profile.view.php
+++ app/views/customer/profile.view.php
@@ -10,6 +10,11 @@
 
   <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+  <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+  <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+  
 </head>
 
 <body>
@@ -106,237 +111,151 @@
   </section> -->
   <?php include 'navigationbar.php'?>
   
-  <section class="main-section" id="main-section">
-    <main id="middle">
-      <div class="profile-picture">
-        <img src="<?= ROOT ?>/assets/images/customer/elon_musk.jpg" class="profile-image" alt="">
-      </div>
-      <div class="edit-pic">
-        <div class="edit-icon">
-          <a href="#">
-            <i class="bx bxs-edit"></i>
-            <span class="link_name">Edit Picture</span>
-          </a>
-        </div>
-      </div>
-      <h1>General Information</h1>
-      <div class="info-gen">
-        <div class="info-type">
-          <div class="profile-info">
-            <div class="info-box">
-              <div class="info-inside">
-                <div class="user-info">
-                  <h1>
-                    Full Name
-                  </h1>
-                </div>
-                <div class="text-info">
-                  <input type="name" name="fullname" value="Elon Reeve Musk" class="edit-gen" readonly>
-                </div>
-                <div class="user-info">
-                  <h1>
-                    City
-                  </h1>
-                </div>
-                <div class="text-info">
-                  <input type="text" name="city" value="Hambanthota" class="edit-gen" readonly>
-                </div>
-                <div class="user-info">
-                  <h1>
-                    Address
-                  </h1>
-                </div>
-                <div class="text-info">
-                  <input type="text" name="address" value="27/A, School Road, Sooriyawewa, Hambanthota" class="edit-gen"
-                    readonly>
-                </div>
-                <div class="user-info">
-                  <h1>
-                    Contact Number
-                  </h1>
-                </div>
-                <div class="text-info">
-                  <input type="text" name="contactno" value="0766464639" class="edit-gen" readonly>
-                </div>
-                <div class="user-info">
-                  <h1>
-                    Date of Birth
-                  </h1>
-                </div>
-                <div class="text-info">
-                  <input type="date" name="birthday" value='2012-12-12' class="edit-gen" readonly>
-                </div>
+  <section class="main" id="main">
+    <div class="wrapper">
 
-                <div class="edit-info-icon">
-                  <a href="#">
-                    <i class="bx bxs-edit"></i>
-                    <span class="link_name" id="gen-info-edit">Edit Info.</span>
-                  </a>
-                </div>
-              </div>
+      <!-- Left Section -->
+      <div class="left">
+        <div class="left-part">
+          <img src="https://i.imgur.com/cMy8V5j.png" alt="user">
+          <h2 class="profile-name">Alex William</h2>
+          <h4 class="profession">Distributor</h4>
+          <div class="edit-pic">
+            <div class="edit-icon">
+              <a href="#">
+                <i class="bx bxs-edit"></i>
+                <span class="link_name">Change Image</span>
+              </a>
             </div>
           </div>
         </div>
       </div>
-      <h1>Security Information</h1>
-      <div class="info-sec">
-        <div class="info-type">
-          <div class="profile-info">
-            <div class="info-box">
-              <div class="info-inside">
-                <div class="user-info">
-                  <h1>
-                    E-mail
-                  </h1>
+
+      <!-- Right Section -->
+      <div class="right">
+        <!-- Information Section -->
+        <div class="info">
+          <h3 class="h3">Personal Information
+            <hr>
+          </h3>
+          <form method="POST">
+
+            <?php if (isset($data)) {
+              ?>
+              <div class="info_data">
+                <div class="data">
+                  <label class="pro_label" for="pro_username"><i class='bx bx-user'></i> Full Name </label>
+                  <input class="pro_input" type="text" id="pro_username" name="emp_name" value=<?= $data->emp_name ?>>
                 </div>
-                <div class="text-info">
-                  <input type="email" name="emailaddress" value="elonmusk@spacex.com" class="edit-gen" readonly>
-                </div>
-                <div class="user-info">
-                  <h1>
-                    Password
-                  </h1>
-                </div>
-                <div class="text-info">
-                  <input type="password" name="password" value="elon123" class="edit-gen" readonly>
-                </div>
-                <div class="change-pw-icon">
-                  <a href="#">
-                    <i class="bx bxs-edit"></i>
-                    <span class="link_name" id="sec-info-edit">Change Password</span>
-                  </a>
+                <div class="data">
+                  <label class="pro_label" for="pro_city"><i class='bx bx-buildings'></i> City</label>
+                  <input class="pro_input" type="text" id="pro_city" name="city" value="<?= $data->city ?>">
                 </div>
               </div>
-            </div>
-          </div>
 
-    </main>
-  </section>
+              <div class="info_data">
+                <div class="data">
+                  <label class="pro_label" for="pro_address">Address</label>
+                  <input class="pro_input" type="text" id="pro_address" name="address"
+                    value="<?= $data->address ?>">
+                </div>
 
-    <!-- Popup -->
-    <section class="popup-section-gen" id="popup-section-gen">
-      <div class="info-pop" id="gen-info-pop">
-        <div class="close-icon">
-          <a href="#">
-            <i class="bx bx-x" id="gen-pop-close"></i>
-            <!-- <span class="link_name">Close</span> -->
-          </a>
+                <div class="data">
+                  <label class="pro_label" for="pro_date">Date of Birth</label>
+                  <input class="pro_input" type="date" id="pro_date" name="DOB" value="<?= $data->DOB ?>">
+                </div>
+              </div>
+
+              <div class="info_data">
+                <div class="data">
+
+                  <label class="pro_label" for="pro_email">Email</label>
+                  <input class="pro_input" type="email" id="pro_email" name="email" value="<?= $data->email ?>">
+                </div>
+                <div class="data">
+                  <label class="pro_label" for="pro_number">Contact Number</label>
+                  <input class="pro_input" type="text" id="pro_number" name="contact_number" value="<?= $data->contact_number ?>">
+                </div>
+
+
+              </div>
+
+              <div class="info_data" id="last-element">
+                <div class="data">
+                  <label class="pro_label" for="pro_profession">Profession</label>
+                  <input class="pro_input" type="text" id="pro_profession" name="emp_status" value="<?= $data->emp_status ?>" disabled>
+                </div>
+                <div class="pro_button">
+                  <button type="button" class="small_btn discard_btn" name="discard" value="discard">Discard</button>
+                  <button type="submit" class="small_btn save_btn" name="save" value="save">Save Changes</button>
+                </div>
+              </div>
+
+
+              <?php
+            }else{
+              redirect("signin");
+            }
+             ?>
+
+          </form>
+
         </div>
-        <div class="info-type">
-          <div class="profile-info">
-            <div class="info-box">
-              <div class="info-inside">
-                <div class="user-info">
-                  <h1>
-                    Full Name
-                  </h1>
-                </div>
-                <div class="text-info">
-                  <input type="text" name="city" required class="edit-popup-gen">
-                </div>
-                <div class="user-info">
-                  <h1>
-                    City
-                  </h1>
-                </div>
-                <div class="text-info">
-                  <input type="text" name="city" required class="edit-popup-gen">
-                </div>
-                <div class="user-info">
-                  <h1>
-                    Address
-                  </h1>
-                </div>
-                <div class="text-info">
-                  <input type="text" name="address" required class="edit-popup-gen">
-                </div>
-                <div class="user-info">
-                  <h1>
-                    Contact Number
-                  </h1>
-                </div>
-                <div class="text-info">
-                  <input type="tel" name="contactno" required class="edit-popup-gen">
-                </div>
-                <div class="user-info">
-                  <h1>
-                    Date of Birth
-                  </h1>
-                </div>
-                <div class="text-info">
-                  <input type="date" name="birthday" required class="edit-popup-gen">
-                </div>
-              
+
+        <!-- Change Password Section -->
+        <div class="info">
+          <h3 class="h3">Security Update
+            <hr>
+          </h3>
+          <form method="POST">
+            <div class="info_data">
+              <div class="data">
+                <label class="pro_label" for="pro_email">Current Password</label>
+                <span class="hide-icon">
+                  <input class="pro_input" type="password" id="c-password" name="password"
+                    placeholder="Enter current password">
+
+                  <a href="#" class="hide active" onclick="togglePasswordVisibility('c-password','s-toggleIcon')">
+                    <ion-icon name="eye-outline" id="s-toggleIcon"></ion-icon>
+                  </a>
+                </span>
+              </div>
+              <div class="data">
+                <label class="pro_label" for="pro_date">New Password</label>
+                <span class="hide-icon">
+
+                  <input class="pro_input" type="password" id="n-password" name="new_password"
+                    placeholder="Enter New password">
+                  <a href="#" class="hide active" onclick="togglePasswordVisibility('n-password','s-toggleIcon')">
+                    <ion-icon name="eye-outline" id="s-toggleIcon"></ion-icon>
+                  </a>
+                </span>
+              </div>
+            </div>
+
+            <div class="info_data" id="last-element">
+              <div class="data">
+                <label class="pro_label" for="pro_profession">Confirm Password</label>
+                <span class="hide-icon">
+
+                  <input class="pro_input" type="text" id="re-password" name="confirm_password"
+                    placeholder="Enter Confirm password">
+                  <a href="#" class="hide active" onclick="togglePasswordVisibility('re-password','s-toggleIcon')">
+                    <ion-icon name="eye-outline" id="s-toggleIcon"></ion-icon>
+                  </a>
+                </span>
+              </div>
+              <div class="pro_button">
+                <button type="button" class="small_btn discard_btn" name="discardP" value="discardP">Discard</button>
+                <button type="submit" class="small_btn save_btn" name="saveP" value="saveP">Save Changes</button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+
   
-              </div>
-              <div class="save-cancel-buttons">
-                <button class="save">Save Changes</button>
-                <button class="cancel" id="gen-edit-cancel">Cancel</button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div class="sec-pop" id="sec-info-pop">
-        <div class="close-icon">
-          <a href="#">
-            <i class="bx bx-x" id="sec-pop-close"></i>
-            <!-- <span class="link_name">Close</span> -->
-          </a>
-        </div>
-        <div class="info-type">
-          <div class="profile-info">
-            <div class="info-box">
-              <div class="info-inside">
-                <div class="user-info">
-                  <h1>
-                    E-mail
-                  </h1>
-                </div>
-                <div class="text-info">
-                  <input type="email" name="emailaddress" value="elonmusk@spacex.com" class="edit-gen">
-                </div>
-                <div class="user-info">
-                  <h1>
-                    Current Password
-                  </h1>
-                </div>
-                <div class="text-info">
-                  <input type="password" name="password" value="elon123" class="edit-gen">
-                </div>
-                <div class="user-info">
-                  <h1>
-                    New Password
-                  </h1>
-                </div>
-                <div class="text-info">
-                  <input type="password" name="newpassword" class="edit-gen" >
-                </div>
-                <div class="user-info">
-                  <h1>
-                    Confirm New Password
-                  </h1>
-                </div>
-                <div class="text-info">
-                  <input type="password" name="confirmnewpassword"  class="edit-gen">
-                </div>
-              </div>
-              <div class="change-pw-icon">
-                <a href="#">
-                  <i class="bx bxs-edit"></i>
-                  <span class="link_name" id="forgot-password">Forgot Password?</span>
-                </a>
-                <div class="pw-save-cancel-buttons">
-                  <button class="save">Save Changes</button>
-                  <button class="cancel" id="sec-edit-cancel">Cancel</button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
     <!-- Scripts -->
     <script src="<?=ROOT?>/assets/js/customer/profile.js"></script>
     <script src="<?=ROOT?>/assets/js/script-bar.js"></script>
