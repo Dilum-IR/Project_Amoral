79032f52de4da526cdc25a6ab0c18b838ca41e28
Sadeep Chathushan <chathushan43sadeep@gmail.com>
2024-01-05 15:52:19 +0530

    Change manager profile and some backend.

--- app/controllers/manager/Profile.php
+++ app/controllers/manager/Profile.php
@@ -6,12 +6,71 @@ class Profile extends Controller
     {
         $username = empty($_SESSION['USER']) ? 'User' : $_SESSION['USER']->email;
 
-        if ($username != 'User') {
+        if ($username != 'User' && $_SESSION['USER']->emp_status === 'manager') {
+
+            $employee = new Employee;
+
+
+            if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['save'])) {
+
+                unset($_POST['save']);
+                // show($_POST);
+                $this->changeInfo($_POST, $_SESSION['USER']->emp_id, $employee);
+            }
+            if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['saveP'])) {
+
+                unset($_POST['saveP']);
+                $this->changePassword($_POST, $_SESSION['USER']->emp_id, $employee);
+                //show($_POST);
+            }
+
+            // get the session user data for bind to the profile info
+            $data = $this->userInfo($_SESSION['USER']->emp_id, $employee);
+
+
+
+            // show($data);
             
-            $this->view('manager/profile');
+            $this->view('manager/profile',$data);
        
         }else{
             redirect('home');
         }
     }
+
+    
+    //  user still database include data 
+    private function userInfo($id, $employee)
+    {
+        $arr['emp_id'] = $id;
+
+        $row = $employee->first($arr);
+
+        unset($row->password);
+        unset($row->emp_id);
+        
+        $data = ['data' => $row];
+        return $data;
+        // show($row);
+
+        // $emprow = $employee->first($arr);
+
+
+
+    }
+    //  user chanage info data
+    private function changeInfo($data, $id, $employee)
+    {
+
+        // show($data);
+
+    }
+
+    //  user chanage password
+    private function changePassword($data, $id, $employee)
+    {
+
+    }
+
 }
+
--- app/views/manager/sidebar.php
+++ app/views/manager/sidebar.php
@@ -69,11 +69,11 @@
             </li>
             <li class="<?=ROOT?>/manager/profile">
                 <div class="profile_details">
-                    <img src="<?=ROOT?>/assets/images/manager/elon_musk.jpg" alt="profile image">
+                    <!-- <img src="<?=ROOT?>/assets/images/manager/elon_musk.jpg" alt="profile image">
                     <div class="profile_content">
                         <div class="name">Elon Musk</div>
                         <div class="designation">Manager</div>
-                    </div>
+                    </div> -->
                 </div>
                 <a href="<?= ROOT ?>/logout">
                     <i class="bx bx-log-out" id="log_out">Log out</i>
