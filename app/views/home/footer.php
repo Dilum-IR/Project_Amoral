4c0ee6bc7c1f7cd9770148115247f1c5460f2ab4
Thiran Sasanka <thiransasanka1@gmail.com>
2024-04-11 22:12:17 +0530

    added a new footer instead of old one, sign up sign in buttons aligned

---
+++ app/views/home/footer.php
@@ -0,0 +1,66 @@
+<footer>
+    <div class="row">
+
+        <div class="col">
+            <img src="./logo.png" class="logo">
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam alias architecto tempore, veritatis
+                nihil ullam tempora possimus nobis rem porro, at perspiciatis nesciunt maiores!</p>
+        </div>
+
+        <div class="col">
+            <h3>Office <div class="underline"><span></span></div>
+            </h3>
+            <p>ITPL Road</p>
+            <p>Whitefield, Bangalore</p>
+            <p>Karnataka, PIN 560066, India</p>
+            <p class="email-id">abc@example.com</p>
+            <h4>+091-0123456789</h4>
+        </div>
+
+        <div class="col">
+            <h3>Links <div class="underline"><span></span></div>
+            </h3>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">See Collection</a></li>
+                <li><a href="#">Design Tool</a></li>
+                <li><a href="#">Place Order</a></li>
+                <li><a href="#">Sign Up</a></li>
+            </ul>
+        </div>
+
+        <div class="col">
+            <h3>Contact Us <div class="underline"><span></span></div>
+            </h3>
+            <form>
+                <div class="e-mail">
+                    <i class="far fa-envelope"></i>
+                    <input type="email" placeholder="Enter your e-mail" required>
+                </div>
+
+                <div class="message">
+                    <i class="far fa-comment-alt"></i>
+                    <input type="text" placeholder="Enter your message" required>
+                </div>
+
+                <div class="send-button">
+                    <div class="hover-text">
+                        Send
+                    </div>
+                    <button type="submit"><i class="fas fa-arrow-right"></i></button>
+
+                </div>
+
+            </form>
+
+            <div class="social-icons">
+                <i class="fab fa-facebook-f"></i>
+                <i class="fab fa-twitter"></i>
+                <i class="fab fa-whatsapp"></i>
+                <i class="fab fa-instagram"></i>
+            </div>
+        </div>
+    </div>
+    <hr>
+    <p class="copyright">Easy Tutorials 2021 - All Rights Reserved</p>
+</footer>
